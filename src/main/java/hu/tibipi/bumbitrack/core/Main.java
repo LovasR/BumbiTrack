39d10e2f4e572c65aa0740afe48d433f4a4e5b8f
LovasR <lovasrdev@gmail.com>
2023-11-13 00:27:54 +0100

    Fixed problem with loading web content before loading UI

--- src/main/java/hu/tibipi/bumbitrack/core/Main.java
+++ src/main/java/hu/tibipi/bumbitrack/core/Main.java
@@ -1,9 +1,7 @@
 package hu.tibipi.bumbitrack.core;
 
 import hu.tibipi.bumbitrack.ui.UIManager;
-import org.json.JSONObject;
 
-import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,30 +13,8 @@ public class Main {
     public static void main(String[] args){
         log.log(Level.INFO, "Initialized");
 
-        String url = "https://maps.nextbike.net/maps/nextbike-live.json?domains=bh";    //this is hardcoded bh, meaning Budapest, Hungary
-        String jsonString = null;
-        try {
-            jsonString = NetUtils.downloadJsonFromURL(url);
-        } catch (IOException e) {
-            log.log(Level.SEVERE, "Couldnt load .json from web");
-            throw new RuntimeException(e);
-        }
-
-        JSONObject rootJSON = new JSONObject(jsonString);
-
-        try{
-            City bp = new City(rootJSON, "MOL Bubi", "Budapest");
-            currentSnap = new Snapshot(bp);
-        } catch (CountryNotFoundException e){
-            log.log(Level.SEVERE, "Specified country not found in file");
-        } catch (CityNotFoundException e){
-            log.log(Level.SEVERE, "Specified city not found in file");
-        }
-
-        if(currentSnap == null){
-            log.log(Level.SEVERE, "Current snapshot is null, exiting");
-            System.exit(1);
-        }
+        Thread initialSnapshotGet = new Thread(Snapshot::createNewSnapshot);
+        initialSnapshotGet.start();
 
         AppUI appUI = new UIManager();
         appUI.start();
--- src/main/java/hu/tibipi/bumbitrack/core/Snapshot.java
+++ src/main/java/hu/tibipi/bumbitrack/core/Snapshot.java
@@ -1,14 +1,24 @@
 package hu.tibipi.bumbitrack.core;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
 
 public class Snapshot {
     private final LocalDateTime dateTime;
     private final City city;
 
+    private static final List<Snapshot> snapshots = new ArrayList<>();
+
     Snapshot(City city){
         dateTime = LocalDateTime.now();
         this.city = city;
+
+        snapshots.add(this);
     }
 
     LocalDateTime getDateTime(){
@@ -17,5 +27,34 @@ public class Snapshot {
 
     public City getCity() {
         return city;
+    }
+
+    //Static parts below
+
+    public static List<Snapshot> getSnapshots() {
+        return snapshots;
+    }
+
+    public static void createNewSnapshot() {
+        String url = "https://maps.nextbike.net/maps/nextbike-live.json?domains=bh";    //this is hardcoded bh, meaning Budapest, Hungary
+        String jsonString;
+        try {
+            jsonString = NetUtils.downloadJsonFromURL(url);
+        } catch (IOException e) {
+            Main.log.log(Level.SEVERE, "Couldnt load .json from web");
+            Main.currentSnap = null;
+            return;
+        }
+
+        JSONObject rootJSON = new JSONObject(jsonString);
+
+        try{
+            City bp = new City(rootJSON, "MOL Bubi", "Budapest");
+            Main.currentSnap = new Snapshot(bp);
+        } catch (CountryNotFoundException e){
+            Main.log.log(Level.SEVERE, "Specified country not found in file");
+        } catch (CityNotFoundException e){
+            Main.log.log(Level.SEVERE, "Specified city not found in file");
+        }
     }
 }
